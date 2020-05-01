418b99d009abe50d43034433bb390b7db303e679
Conor Byrne <con0r.b@protonmail.com>
2020-05-01 12:18:57 +0100

    Add 1.12.2 support

--- src/main/java/io/github/tivj/textless/ToggleCommand.java
+++ src/main/java/io/github/tivj/textless/ToggleCommand.java
@@ -1,28 +1,45 @@
 package io.github.tivj.textless;
 
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
 
 public class ToggleCommand extends CommandBase {
     public static boolean textEnabled = true;
 
     @Override
-    public String getCommandName() {
+    public int getRequiredPermissionLevel() {
+        return -1;
+    }
+
+    /**
+     * Gets the name of the command
+     */
+    @Override
+    public String getName() {
         return "texttoggle";
     }
 
+    /**
+     * Gets the usage string for the command.
+     *
+     * @param sender
+     */
     @Override
-    public String getCommandUsage(ICommandSender sender) {
+    public String getUsage(ICommandSender sender) {
         return "/texttoggle";
     }
 
+    /**
+     * Callback for when the command is executed
+     *
+     * @param server
+     * @param sender
+     * @param args
+     */
     @Override
-    public void processCommand(ICommandSender sender, String[] args) {
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
         textEnabled =! textEnabled;
-    }
-
-    @Override
-    public int getRequiredPermissionLevel() {
-        return -1;
     }
 }
